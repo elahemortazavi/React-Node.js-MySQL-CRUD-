196e1290720ee9b8509447d5dbb07c762379698b
elahemortazavi <emor5680@gmail.com>
2023-06-08 14:51:17 +0100

    react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
 
 function App() {
   return (
     <div className="App">
-     hello
+      <BrowserRouter>
+        <Routes>
+          <route path="/" element={<Books />} />
+          <route path="/add" element={<Add />} />
+          <route path="/update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
